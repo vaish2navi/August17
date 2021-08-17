440d10b6c703747d3e6a05e795c1edb761b225cc
Ram Kumar <vaish2navi>
2021-08-17 22:54:43 +1200

    updated successfully

---
+++ src/com/Git/com/Demo.java
@@ -0,0 +1,17 @@
+package com.Git.com;
+
+public class Demo 
+{
+	public static void main(String[] args) {
+		System.out.println("git demo project");
+		
+		System.out.println("git demo project");
+		
+		System.out.println("git demo project");
+		
+		System.out.println("git demo project");
+		
+		
+		
+	}
+}
